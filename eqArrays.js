a96fa53a9c46538caf773a24896c98ec8f6724d3
May C <maycheongs@gmail.com>
2021-01-05 21:05:14 +0000

    added eqArray function

---
+++ eqArrays.js
@@ -0,0 +1,15 @@
+const assertEqual = require('./assertequal.js');
+
+const eqArrays = function(firstArray, secArray) {
+  if (firstArray.length !== secArray.length) {
+    return false;
+  }
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secArray[i]) {
+      return false;
+    }
+  } return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
