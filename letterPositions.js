54d220fc32a5da148d3c35492a24097865fe8c72
May C <maycheongs@gmail.com>
2021-01-06 22:39:39 +0000

    adds array of indexes as value according to letter key in a sentence

---
+++ letterPositions.js
@@ -0,0 +1,32 @@
+const eqArrays = function(firstArray, secArray) {
+  if (firstArray.length !== secArray.length) {
+    return false;
+  }
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secArray[i]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  eqArrays(actual, expected) ? console.log(`✅Assertion Passed: ${actual} is equal ${expected}`) : console.log(`❗Assertion Failed: ${actual} does not equal ${expected} (Check Type)`);
+
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+
+  for (let index in sentence) {
+    index = Number(index);
+    let letter = sentence[index];
+    results[letter] ? results[letter].push(index) : results[letter] = [index];
+  }
+
+  delete results[' '];
+  return results;
+
+};
+
+assertArraysEqual(letterPositions("lighthouse in the house").h, [3,5,15,18]);
+//assertEqual(letterPositions("lighthouse in the house")[' '], undefined)
