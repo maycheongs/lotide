42db2e6b730249eedc87b480b9e41d80a67c1879
May C <maycheongs@gmail.com>
2021-01-09 08:54:42 +0000

    commented console logs

--- eqArrays.js
+++ eqArrays.js
@@ -20,9 +20,9 @@ const eqArrays = function(firstArray, secArray) {
   return true;
 };
 
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+/* assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
 assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
 assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
-
+ */
 
 
