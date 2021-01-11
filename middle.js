6ae99c0c45497896dbbc3e75a9e5e3c2e6193c87
May C <maycheongs@gmail.com>
2021-01-11 18:23:31 +0000

    refactor middle and middleTest

--- middle.js
+++ middle.js
@@ -1,21 +1,5 @@
-const eqArrays = function(firstArray, secArray) {
-  if (firstArray === secArray) {
-    return true;
-  }
-  if (firstArray.length !== secArray.length) {
-    return false;
-  }
-  for (let i = 0; i < firstArray.length; i++) {
-    if (firstArray[i] !== secArray[i]) {
-      return false;
-    }
-  } return true;
-};
 
-const assertArraysEqual = function(actual, expected) {
-  eqArrays(actual, expected) ? console.log(`✅Assertion Passed: ${actual} is equal ${expected}`) : console.log(`❗Assertion Failed: ${actual} does not equal ${expected} (Check Type)`);
 
-};
 
 const middle = array => {
   let result = [];
@@ -31,9 +15,5 @@ const middle = array => {
 };
 
 
+module.exports = middle;
 
-
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1,2]), []);
-assertArraysEqual(middle([1,2,3]), [2]);
-assertArraysEqual(middle([1,2,3,4]), [2,3]);
