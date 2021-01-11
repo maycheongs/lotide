720efb095f6d1baba504ec51256a3c7db1e829b3
May C <maycheongs@gmail.com>
2021-01-11 17:42:35 +0000

    exported assertequal and added test folder

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,9 @@
+
+
 const assertEqual = function(actual, expected) {
   actual === expected ? console.log(`✅Assertion Passed: ${actual} === ${expected}`) : console.log(`❗Assertion Failed: ${actual} !== ${expected}`);
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
 
+
+ module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,11 +1,6 @@
-const assertEqual = function(actual, expected) {
-  actual === expected ? console.log(`✅Assertion Passed: ${actual} === ${expected}`) : console.log(`❗Assertion Failed: ${actual} !== ${expected}`);
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   return array[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
